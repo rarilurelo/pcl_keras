8298090f83d2838b08f0e4844077e76ebd433ec3
rarilurelo <rarilurelo@gmail.com>
2017-03-09 21:37:32 +0900

    fix

--- replay_buffer.py
+++ replay_buffer.py
@@ -3,13 +3,14 @@ import numpy as np
 class ReplayBuffer(object):
     def __init__(self, max_len=100000, alpha=1):
         self.max_len = max_len
+        self.alpha = alpha
         self.buffer = []
         # weight is not normalized
         self.weight = np.array([])
 
     def add(self, episode):
         self.buffer.append(episode)
-        self.weight = np.append(self.weight, np.exp(alpha*episode['rewards'].sum()))
+        self.weight = np.append(self.weight, np.exp(self.alpha*episode['rewards'].sum()))
         if len(self.buffer) > self.max_len:
             delete_ind = np.random.randint(len(self.buffer))
             del self.buffer[delete_ind]
